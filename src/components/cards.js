31408c037d3c5c69ab5b42cfe074e3c19c168899
Danil Sitdikov <daniltro1@yandex.ru>
2024-03-16 15:15:37 +0300

    add func (modal, add new card, like card and edit profile

---
+++ src/components/cards.js
@@ -0,0 +1,53 @@
+export const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+export function addCard(card, callBackRemove, callBackLike, openPopup) {
+  const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".places__item");
+  const cardClone = cardTemplate.cloneNode(true);
+  const cardTitle = cardClone.querySelector(".card__title");
+  cardTitle.textContent = card.name;
+  const cardImage = cardClone.querySelector(".card__image");
+  cardImage.setAttribute("src", card.link);
+  cardImage.setAttribute("alt", card.name);
+  const deleteButton = cardClone.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", callBackRemove);
+  const likeButton = cardClone.querySelector(".card__like-button");
+  likeButton.addEventListener("click", callBackLike);
+  const image = cardClone.querySelector(".card__image");
+  image.addEventListener("click", openPopup);
+  return cardClone;
+}
+
+export function cardLike(evt) {
+  evt.target.classList.toggle("card__like-button_is-active");
+}
+
+export function removeCard(event) {
+  event.target.closest(".places__item").remove();
+}
---
+++ src/components/index.js
@@ -0,0 +1,99 @@
+// @todo: Темплейт карточки
+// @todo: DOM узлы
+// @todo: Функция создания карточки
+// @todo: Функция удаления карточки
+// @todo: Вывести карточки на страницу
+
+import "../styles/index.css";
+import { initialCards } from "./cards.js";
+import { addCard } from "./cards.js";
+import { cardLike } from "./cards.js";
+import { removeCard } from "./cards.js";
+import { openPopup } from "./modal.js";
+import { closePopup } from "./modal.js";
+import { openCard } from "./modal.js";
+import { addNewCard } from "./modal.js";
+
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach(function (card) {
+  card = addCard(card, removeCard, cardLike, openCard);
+  placesList.appendChild(card);
+});
+
+// ----------------------------- модальные окна------------------------------------------------
+
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addProfileButton = document.querySelector(".profile__add-button");
+const editProfilePopup = document.querySelector(".popup_type_edit");
+const newCardPopup = document.querySelector(".popup_type_new-card");
+const popupTypeImage = document.querySelector(".popup_type_image");
+const popups = document.querySelectorAll(".popup");
+
+export function escHandler(evt) {
+  console.log(evt.key);
+  if (evt.key === "Escape") {
+    if (editProfilePopup.classList.contains("popup_is-opened")) {
+      closePopup(editProfilePopup);
+    }
+    if (newCardPopup.classList.contains("popup_is-opened")) {
+      closePopup(newCardPopup);
+    }
+    if (popupTypeImage.classList.contains("popup_is-opened")) {
+      closePopup(popupTypeImage);
+    }
+  }
+}
+
+const formAddProfile = document.forms["new-place"];
+const placeName = formAddProfile.elements["place-name"];
+const placeLink = formAddProfile.elements["link"];
+
+export {placesList, placeName, placeLink};
+
+formAddProfile.addEventListener("submit", addNewCard);
+
+editProfileButton.addEventListener("click", () => {
+  openPopup(editProfilePopup);
+});
+
+addProfileButton.addEventListener("click", () => {
+  openPopup(newCardPopup);
+});
+
+popups.forEach((popup) => {
+  popup.classList.add("popup_is-animated");
+  popup.addEventListener("click", (evt) => {
+    if (evt.target === popup) {
+      closePopup(evt.target.closest(".popup"));
+    }
+    if (evt.target.classList.contains("popup__close")) {
+      closePopup(evt.target.closest(".popup"));
+    }
+  });
+});
+
+// -------------------- Редактирование имени и информации о себе-------------------------------------------------
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const formEditProfile = document.forms["edit-profile"];
+
+formEditProfile.elements["name"].value = profileTitle.textContent;
+formEditProfile.elements["description"].value = profileDescription.textContent;
+
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_description");
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  profileTitle.textContent = name;
+  profileDescription.textContent = job;
+  closePopup(evt.target.closest(".popup"));
+}
+
+formEditProfile.addEventListener("submit", handleFormSubmit);
